fe7b510da3f75cc65edd4edd77ffd40ea6df44f4
Fabio Kreusch <fabiokr@gmail.com>
2011-05-28 12:43:47 -0700

    Generated base app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Mdmanagement::Application.config.secret_token = '854073efcc334d1f7942e803097bad38a0bc44df5d94f79455ad3861d60388186dc3c1dad1067490c0a0da975d075386b2f5f4fa02ac08796eea58576c7b6f61'
