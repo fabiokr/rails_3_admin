f456cc9f203335abd025776bcc51df2159f4feb9
Fabio Kreusch <fabiokr@gmail.com>
2011-06-09 12:30:46 -0700

    Changed breadcrumb helper

--- lib/admin/helpers/breadcrumb_builder.rb
+++ lib/admin/helpers/breadcrumb_builder.rb
@@ -3,9 +3,12 @@ module Admin
     class BreadcrumbBuilder < BreadcrumbsOnRails::Breadcrumbs::SimpleBuilder
 
       def render_element(element)
-        content = @context.link_to_unless_current(compute_name(element), compute_path(element)) do
+        content = if element == @elements.last
           @context.content_tag(:a, compute_name(element), :class => 'current')
+        else
+          @context.link_to(compute_name(element), compute_path(element))
         end
+
         if @options[:tag]
           @context.content_tag(@options[:tag], content)
         else
